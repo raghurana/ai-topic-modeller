e48917508d682570f68d66159a571ebff67d9341
Raghu Rana <raghurana@gmail.com>
2025-03-31 22:41:03 +1100

    Move PostgreSQL client initialization into the main function for better encapsulation

--- backend/cli.ts
+++ backend/cli.ts
@@ -3,9 +3,8 @@ import { readFile } from 'fs/promises';
 import { Utils } from './utils';
 import { TopicsProcessor } from './topics-processor';
 
-const pgClient = Utils.pg.newClient();
-
 const main = async () => {
+  const pgClient = Utils.pg.newClient();
   await pgClient.connect();
   const testCsvFile = join(__dirname, '_csv', '01-topics.csv');
   const systemPrompt = await readFile(join(__dirname, 'system-prompt.txt'), 'utf-8');
